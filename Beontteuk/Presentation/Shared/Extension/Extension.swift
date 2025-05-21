4a427b75c74e54c83289d194b6cac007e7d3096c
Yimkeul <leesjun29@gmail.com>
2025-05-21 11:53:40 +0900

    feat: #6 - 배경색 Gradient 설정, BaseView, BaseController에 적용

--- Beontteuk/Presentation/Shared/Extension/Extension.swift
+++ Beontteuk/Presentation/Shared/Extension/Extension.swift
@@ -23,6 +23,29 @@ extension UIView {
             self.addSubview($0)
         }
     }
+
+    func applyGradient() {
+        let colors:[UIColor] = [.background200, .background500]
+        let startPoint = CGPoint(x: 0, y: 0)
+        let endPoint = CGPoint(x: 1, y: 1)
+        let locations: [NSNumber]? = nil
+
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = colors.map(\.cgColor)
+        gradient.locations = locations
+        gradient.startPoint = startPoint
+        gradient.endPoint = endPoint
+
+        layer.insertSublayer(gradient, at: 0)
+    }
+
+    /// layoutSubviews 등에서 크기가 바뀔 때마다 다시 적용할 수 있게
+    func refreshGradient() {
+        layer.sublayers?
+            .compactMap { $0 as? CAGradientLayer }
+            .forEach { $0.frame = bounds }
+    }
 }
 
 extension UIStackView {
