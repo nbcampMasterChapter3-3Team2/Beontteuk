350b8a6ba109f8dd98308df304bc7fbc515b0fa2
Yimkeul <leesjun29@gmail.com>
2025-05-23 16:43:57 +0900

    feat: #25 - bottom Sheet에 있는 테이블 폴더 이동

--- Beontteuk/Presentation/Alarm/Cell/AlarmBottomSheetTableViewCell.swift
+++ Beontteuk/Presentation/Alarm/Cell/AlarmBottomSheetTableViewCell.swift
@@ -126,47 +126,5 @@ final class AlarmBottomSheetTableViewCell: BaseTableViewCell {
             toggleSwitch.isHidden = false
         }
     }
-
-    // MARK: - Actions
-    @objc private func switchValueChanged(_ sender: UISwitch) {
-        snoozeChanged?(sender.isOn)
-    }
 }
 
-extension AlarmBottomSheetTableViewCell {
-    enum Option: CaseIterable {
-        case `repeat`, label, sound, snooze
-
-        var title: String {
-            switch self {
-            case .repeat: return "반복"
-            case .label: return "레이블"
-            case .sound: return "사운드"
-            case .snooze: return "다시 알림"
-            }
-        }
-
-        var detailText: String {
-            switch self {
-            case .repeat: return "안 함"
-            case .label: return "알람"
-            case .sound: return "노래제목"
-            case .snooze: return ""
-            }
-        }
-    }
-}
-
-extension AlarmBottomSheetTableViewCell: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        labelChanged?(textField.text)
-    }
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        labelChanged?("")
-        return true
-    }
-}
